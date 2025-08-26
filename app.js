4717d224f5abb9dcf20eb4d539bf0a067544c3d7
meharahmadft6 <167222938+meharahmadft6@users.noreply.github.com>
2025-08-26 20:07:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -36,19 +36,6 @@ app.use(
   })
 );
 
-// Ensure headers on ALL responses (fallback)
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3001",
-      "https://infinityquotientlearning.vercel.app",
-      "https://infinityquotientlearning.com",
-      "https://www.infinityquotientlearning.com",
-    ],
-    credentials: true,
-  })
-);
-
 // Body parser
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
