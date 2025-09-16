a78b91b29ec1398dd2ff837446b46695acd79dc8
meharahmadft6 <167222938+meharahmadft6@users.noreply.github.com>
2025-09-16 19:55:57 +0500

    updated fixes

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -76,8 +76,8 @@ exports.getTeacher = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: {
-      ...teacherWithUrls.toObject(),
-      walletBalance: balance, // ✅ Added wallet balance
+      ...teacherWithUrls, // no .toObject() here
+      walletBalance: balance,
     },
   });
 });
